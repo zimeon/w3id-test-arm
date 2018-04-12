51165881e5b2249f6c0afef613b55969ab64b78f
Simeon Warner <simeon.warner@cornell.edu>
2018-04-11 20:11:30 -0400

    A start

---
+++ README.md
@@ -0,0 +1,5 @@
+# Local Mac OSX Apache setup to test https://w3id.org/arm config
+
+  * Assumes checked out copy of config in `~/src/w3id.org/`
+  * Creates Apache root in `/tmp/w3id-test-arm` (zaps anything with that name)
+  * Run with `./
---
+++ run_test_apache_mac.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Cleanup
+#killall -9 httpd
+rm -rf /tmp/w3id-test-arm
+
+# Setup
+mkdir /tmp/w3id-test-arm
+mkdir /tmp/w3id-test-arm/logs
+mkdir /tmp/w3id-test-arm/htdocs
+cp ~/src/w3id.org/.htaccess /tmp/w3id-test-arm
+cp -r ~/src/w3id.org/arm /tmp/w3id-test-arm/htdocs
+
+# Run
+echo "Running under on http://localhost:8080/arm/"
+httpd -d . -f apache.conf
+sleep 1
+pid=`cat /tmp/w3id-test-arm/logs/httpd.pid`
+echo "Running with PID $pid"
+echo "Tailing error and access logs... (^C to exit,  kill -TERM $pid   to clean up)"
+echo "Try: http://localhost:8080/arm/BAD"
+echo "Try: http://localhost:8080/arm"
+echo "Try: http://localhost:8080/arm/"
+echo "Try: http://localhost:8080/arm/ontologies/core"
+tail -qf /tmp/w3id-test-arm/logs/error_log /tmp/w3id-test-arm/logs/access_log
+
